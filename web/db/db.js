e22354fb600c27524ef3bd0bbed7e16a3e330813
priyapatel-34 <priyav.patel@tatvasoft.com>
2026-05-20 17:46:22 +0530

    Changes in DB configuration

--- web/db/db.js
+++ web/db/db.js
@@ -18,7 +18,7 @@ export const pool = new Pool({
   host: process.env.DATABASE_HOST,
   user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
-  database: process.env.PG_DATABASE,
+  database: process.env.DATABASE_NAME,
   port: process.env.DATABASE_PORT || 5432,
   ssl:
     process.env.DATABASE_SSL === "true"
